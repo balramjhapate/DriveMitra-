07023b5c0688ba79b477184f8a6776a65ae023ec
Developer <developer@drivemitra.com>
2026-08-09 16:33:26 +0530

    feat: integrate Microsoft Clarity tracking ID

--- app/layout.tsx
+++ app/layout.tsx
@@ -89,7 +89,7 @@ export default function RootLayout({
                 c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "clarity-project-id");
+            })(window, document, "clarity", "script", "xzndflcba9");
           `}
         </Script>
 
